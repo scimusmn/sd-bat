81c8ce7317bc85848ef3f95fae5d4981a4ead4ed
bryan kennedy <bkennedy@smm.org>
2014-11-22 15:05:40 -0600

    Propoer spacing

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -9,26 +9,26 @@ Client-side Router.
 // Config
 
 Router.configure({
-  layoutTemplate: 'layout',
-  loadingTemplate: 'loading',
-  notFoundTemplate: 'notFound',
-  trackPageView: true
+    layoutTemplate: 'layout',
+    loadingTemplate: 'loading',
+    notFoundTemplate: 'notFound',
+    trackPageView: true
 });
 
 // Filters
 
 var filters = {
 
-  myFilter: function () {
-    // do something
-  },
+    myFilter: function () {
+        // do something
+    },
 
-  isLoggedIn: function() {
-    if (!(Meteor.loggingIn() || Meteor.user())) {
-      alert('Please Log In First.')
-      this.stop();
+    isLoggedIn: function() {
+        if (!(Meteor.loggingIn() || Meteor.user())) {
+            alert('Please Log In First.')
+            this.stop();
+        }
     }
-  }
 
 }
 
@@ -38,46 +38,46 @@ Router.onBeforeAction(filters.myFilter, {only: ['items']});
 
 Router.map(function() {
 
-  // Items
+    // Items
 
-  this.route('items', {
-    waitOn: function () {
-      return Meteor.subscribe('allItems');
-    },
-    data: function () {
-      return {
-        items: Items.find()
-      }
-    }
-  });
+    this.route('items', {
+        waitOn: function () {
+            return Meteor.subscribe('allItems');
+        },
+        data: function () {
+            return {
+                items: Items.find()
+            }
+        }
+    });
 
-  this.route('item', {
-    path: '/items/:_id',
-    waitOn: function () {
-      return Meteor.subscribe('singleItem', this.params._id);
-    },
-    data: function () {
-      return {
-        item: Items.findOne(this.params._id)
-      }
-    }
-  });
+    this.route('item', {
+        path: '/items/:_id',
+        waitOn: function () {
+            return Meteor.subscribe('singleItem', this.params._id);
+        },
+        data: function () {
+            return {
+                item: Items.findOne(this.params._id)
+            }
+        }
+    });
 
 
-  // Pages
+    // Pages
 
-  this.route('homepage', {
-    path: '/'
-  });
+    this.route('homepage', {
+        path: '/'
+    });
 
-  this.route('content');
+    this.route('content');
 
-  // Users
+    // Users
 
-  this.route('login');
+    this.route('login');
 
-  this.route('signup');
+    this.route('signup');
 
-  this.route('forgot');
+    this.route('forgot');
 
 });
