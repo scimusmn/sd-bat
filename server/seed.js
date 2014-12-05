37f4c43aa49c9b538a1937e24b72465a00bb0c00
bryan kennedy <bkennedy@smm.org>
2014-12-04 23:22:21 -0600

    Updated copy with real Spanish

--- server/seed.js
+++ server/seed.js
@@ -13,40 +13,40 @@ if (Bats.find().count() === 0) {
     _id: 'townsend-big-ear',
     order: 5,
     name: "Townsend's<br>Big-eared Bat",
-    nameEs: "Corynorhinus townsendii",
+    nameEs: "Murciélago orejón de Townsend",
     nameLatin: "Corynorhinus townsendii",
-    body: " <p>This call starts so fast it almost sounds like a machine gun.</p> <p>See if you can follow along as it ends abruptly in a few slower thuds.</p> ",
-    bodyEs: " <p>Esta llamada comienza tan rápido que casi suena como una ametralladora.</p> <p>Vea si usted puede seguir a lo largo ya que termina abruptamente en pocos golpes más lentos.</p>"
+    body: "<p>Watch the graph as this speedy call starts, then ends abruptly in a few slower thuds.</p>",
+    bodyEs: "<p>Mira el gráfico al empezar esta veloz llamada, que luego cesa abruptamente en unos pocos golpes sordos más lentos.</p>"
 });
 
   Bats.insert({
     _id: 'yuma-myotis',
     order: 1,
     name: "Yuma Myotis",
-    nameEs: "Yuma Myotis",
+    nameEs: "Miotis de Yuma",
     nameLatin: "Myotis yumanensis",
-    body: " <p>Quick and high, this call starts out strong.<br>Follow along on the graph as it comes to a halt then picks back up.</p>",
-    bodyEs: " <p>Rápida y alta, esta llamada comienza fuerte. Seguir a lo largo de la gráfica, ya que llega a su fin luego toma una copia de seguridad.</p>"
+    body: "<p>Follow along on the graph as this high, rapid call comes to a halt, then starts back up.</p>",
+    bodyEs: "<p>Sigue el gráfico mientras se desarrolla para ver cómo esta aguda y veloz llamada se detiene abruptamente, y luego comienza otra vez.</p>"
   });
 
   Bats.insert({
     _id: 'mexican-free-tailed',
     order: 4,
     name: "Mexican<br>Free-tailed Bat",
-    nameEs: "Mexicano de cola libre Bat",
+    nameEs: "Murciélago cola-suelta brasileño",
     nameLatin: "Tadarida brasiliensis",
-    body: "<p>The chirps of this call sound a bit like a bird.</p> <p>See if you can pick out spots where it gets faster and louder on the graph.</p>",
-    bodyEs: "<p>Los chirridos de esta convocatoria suenan un poco como un pájaro.</p> <p>Vea si usted puede seleccionar los puntos donde se hace más rápido y más fuerte en el gráfico.</p>",
+    body: "<p>See if you can pick out spots on the graph where the short chirps of this call get faster and louder.</p>",
+    bodyEs: "<p>Fíjate a ver si puedes detectar los puntos en el gráfico donde los breves chirridos de esta llamada aumentan en velocidad y volumen.</p> ",
   });
 
   Bats.insert({
     _id: 'canyon-bat',
     order: 2,
     name: "Canyon Bat",
-    nameEs: "Cañón Bat",
+    nameEs: "Pipistrelo del oeste americano",
     nameLatin: "Parastrellus hesperus",
-    body: " <p>This call gets higher than any of the other calls.</p> <p>Look for extreme changes in its speed and volume on the graph below.</p>",
-    bodyEs: " <p>Este llamado se hace mayor que cualquiera de las otras llamadas.</p> <p>Mira tfor cambios extremos en su velocidad y el volumen en la siguiente gráfica .</p>",
+    body: "<p>Find the one place on the graph where this rapid call stops before racing along again.</p>",
+    bodyEs: "<p>Encuentra el punto en el gráfico donde esta rápida llamada se detiene antes de comenzar velozmente otra vez.</p>",
   });
 
   Bats.insert({
@@ -55,8 +55,8 @@ if (Bats.find().count() === 0) {
     name: "Western Mastiff Bat",
     nameEs: "Western mastín Bat",
     nameLatin: "Eumops perotis",
-    body: " <p>One of the few bats that calls slow and low enough for people to hear without special equipment.</p> <p>Watch the graph to see how it begins slower and lower than any of the other calls before it picks up speed.</p>",
-    bodyEs: " <p>Uno de los pocos murciélagos que llama lenta y lo suficientemente bajo como para que la gente escuche sin equipo especial .</p> <p>Vea el gráfico para ver cómo empieza lento y menor que cualquiera de las otras llamadas antes de que se acelera .</p>",
+    body: "<p>One of the few bats with a call slow and low enough for people to hear without special equipment. Watch it progress on the graph from low to high.</p>",
+    bodyEs: "<p>Uno de los pocos murciélagos con una llamada lenta y lo suficientemente baja en frecuencia para que las personas la oigan sin equipos especiales. Mira cómo se desarrolla en el gráfico de grave a agudo.</p> ",
   });
 
 }
