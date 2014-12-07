0cc6cdfbe3ace4bbabeafce98eb14b6ee0c207ba
bryan kennedy <bkennedy@smm.org>
2014-12-07 10:51:18 -0600

    Enabling screensaver detection on hoempage

--- client/views/bats/bats.js
+++ client/views/bats/bats.js
@@ -14,8 +14,16 @@ Template.bats.helpers({
     //
 });
 
-Template.bats.rendered = function () {
-    //
+Template.bats.rendered = function() {
+    /**
+     * Define without the var to make this accessible across the events
+     */
+    saverEnabled = Router.current().params.query.saver;
+    if (saverEnabled) {
+        $('#screensaver').removeClass('animated fadeOut');
+        $('#screensaver').addClass('animated fadeIn');
+        $('#screensaver').show();
+    }
 };
 
 Template.bats.events({
