3177ccb57b69d63092ef6d5d880a1d3337c610ab
bryan kennedy <bkennedy@smm.org>
2014-12-07 10:50:29 -0600

    Video playback controlls
    This gives you the ability to pause/play the video and replay it when it
    ends.

--- client/views/bats/bat.js
+++ client/views/bats/bat.js
@@ -19,14 +19,42 @@ Template.bat.helpers({
 });
 
 Template.bat.rendered = function () {
-  //
+    var video = $('#bat-video')[0];
+    video.onended = function(e) {
+        $('#bat-video')[0].pause();
+        video.currentTime = 0;
+        $('.video-control .pause').addClass('disabled');
+        $('.video-control .play').removeClass('disabled');
+    };
 };
 
 Template.bat.events({
 
+    'end #bat-video': function(e) {
+        console.log('Video end');
+    },
+
+    'click .video-control .pause': function(e) {
+        e.preventDefault();
+        console.log('Pause button clicked.');
+        $('#bat-video')[0].pause();
+        $('.video-control .pause').addClass('disabled');
+        $('.video-control .play').removeClass('disabled');
+    },
+
+    'click .video-control .play': function(e) {
+        e.preventDefault();
+        console.log('Play button clicked.');
+        $('#bat-video')[0].play();
+        $('.video-control .play').addClass('disabled');
+        $('.video-control .pause').removeClass('disabled');
+    },
+
     'click .btn-home': function(e) {
         e.preventDefault();
-        Router.go('/');
+        Router.go('/bats');
     }
 
 });
+
+8
